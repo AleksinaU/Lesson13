e86356cf76599a649ad0c128bbebc5da8c56e87d
Юлия Алексина <ulchez@mail.ru>
2022-07-28 23:28:23 +0300

    Добавьте файлы проекта.

---
+++ Lesson13/MultiBuilding.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13
+{
+    sealed class MultiBuilding : Building
+    {
+        int floor;
+        int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value < 0)
+                    floor = 0;
+                else
+                    floor = value;
+            }
+        }
+
+        public MultiBuilding(string address, double lenght, double width, double height, int floor)
+        :base (address, lenght, width, height)
+        {
+            this.Floor = floor; 
+        }
+        public string Print()
+        {
+            string result = base.Print();
+            result += $" {floor}";
+            return result;
+        }
+
+    }
+}
---
+++ Lesson13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new Building("проезд Оптимистов", 12000, 12000, 6000);
+            Console.WriteLine(building.Print());
+            MultiBuilding multiBuilding = new MultiBuilding("проезд Оптимистов", 12000, 12000, 6000 , 1);
+            Console.WriteLine(multiBuilding.Print());
+            Console.ReadKey();
+        }
+    }
+}
